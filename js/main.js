0c9cb005ccf0f3b86d27136cee5f9f8625cb9835
MartinEden <58919772+sava911@users.noreply.github.com>
2020-06-09 05:26:33 +0300

    my page document

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+ $(`.slider-inner, .news-slider-inner`).slick({
+ 	nextArrow: `<button type="button" class="slick-btn slick-next"></button>`,
+ 	prevArrow: `<button type="button" class="slick-btn slick-prev"></button>`,
+ 	infinite: false
+ });
+
+
+ $(`select`).styler();
+
+
+});
